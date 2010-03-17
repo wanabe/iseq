90cfbaf0d606391c6e077274b97c5c90a756f3be
wanabe <s.wanabe@gmail.com>
2010-03-18 00:28:50 +0900

    * change API name by RUBY_VERSION.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,6 +1,21 @@
 require "mkmf"
+
+# $defs = ["-D_MSC_VER=1200"]
+# $LDFLAGS += " msvcrt_winxp.obj"
+
 if RUBY_VERSION >= "1.9.0" && 
    defined?(RUBY_ENGINE) &&
    RUBY_ENGINE == "ruby"
+  #$defs ||= []
+  load_api = \
+  if RUBY_VERSION < "1.9.1"
+    $CFLAGS += " -DOLD_ISEQ_LOAD"
+    "rb_iseq_load"
+  elsif RUBY_VERSION < "1.9.2"
+    "ruby_iseq_load"
+  else
+    "rb_iseq_load"
+  end
+  $CFLAGS += " -DISEQ_LOAD=#{load_api}"
   create_makefile("iseq")
 end
--- ext/iseq.c
+++ ext/iseq.c
@@ -1,7 +1,20 @@
 #include "ruby.h"
+#ifndef ISEQ_LOAD
+#define ISEQ_LOAD rb_iseq_load
+#endif
 
 RUBY_EXTERN VALUE rb_cISeq;
-RUBY_EXTERN VALUE rb_iseq_load(VALUE data, VALUE parent, VALUE opt);
+
+#ifdef OLD_ISEQ_LOAD
+RUBY_EXTERN VALUE iseq_load(VALUE self, VALUE data, VALUE parent, VALUE opt);
+static inline VALUE
+ISEQ_LOAD(VALUE data, VALUE parent, VALUE opt)
+{
+    iseq_load(rb_cISeq, data, parent, opt);
+}
+#else
+RUBY_EXTERN VALUE ISEQ_LOAD(VALUE data, VALUE parent, VALUE opt);
+#endif
 
 static VALUE
 iseq_s_load(int argc, VALUE *argv, VALUE self)
@@ -9,7 +22,7 @@ iseq_s_load(int argc, VALUE *argv, VALUE self)
     VALUE data, opt=Qnil;
     rb_scan_args(argc, argv, "11", &data, &opt);
 
-    return rb_iseq_load(data, 0, opt);
+    return ISEQ_LOAD(data, 0, opt);
 }
 
 void
